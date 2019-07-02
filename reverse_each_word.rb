db3ba0898c55dc5611c979769a36cdb812d31c8c
Jonathan Koss <jonathan_koss@yahoo.com>
2019-07-02 00:13:33 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,7 @@
 def reverse_each_word(sentence)
-  array = sentence.split(" ")
-  array.collect do |word| 
+  split_array = sentence.split(" ")
+  reversed_words = split_array.collect do |word| 
     word.reverse
   end 
+  reversed_words.join(" ")
 end 
